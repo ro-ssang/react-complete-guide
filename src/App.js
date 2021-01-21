9cb73406e7e236cc1b19a4f00a5bdc8caf77b83f
rossa <fashionfish@naver.com>
2021-01-21 12:56:07 +0900

    Rendering content conditionally

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ class App extends Component {
         { name: "Manu", age: 29 },
         { name: "Stephanie", age: 27 },
       ],
+      showPersons: false,
     });
   };
 
@@ -32,6 +33,11 @@ class App extends Component {
         { name: "Stephanie", age: 26 },
       ],
     });
+  };
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
   };
 
   render() {
@@ -47,14 +53,18 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button style={style} onClick={() => this.switchNameHandler("Maximilian!!")}>
+        <button style={style} onClick={this.togglePersonsHandler}>
           Switch Name
         </button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler.bind(this, "Max!")} changed={this.nameChangedHandler}>
-          My hobbies: Racing
-        </Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        {this.state.showPersons ? (
+          <div>
+            <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+            <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler.bind(this, "Max!")} changed={this.nameChangedHandler}>
+              My hobbies: Racing
+            </Person>
+            <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+          </div>
+        ) : null}
       </div>
     );
     // return React.createElement("div", { className: "App" }, React.createElement("h1", null, "Does this work now?"));
