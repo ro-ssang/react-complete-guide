bbf562061e560b639296cf0566fe11d3226abfb6
rossa <fashionfish@naver.com>
2021-01-22 21:45:47 +0900

    Higher Order Components (HOC) - Introduction

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import classes from "./App.css";
 import Cockpit from "../components/Cockpit/Cockpit";
 import Persons from "../components/Persons/Persons";
+import WithClass from "../hoc/WithClass";
 
 class App extends Component {
   constructor(props) {
@@ -103,11 +104,11 @@ class App extends Component {
     }
 
     return (
-      <div className={classes.App}>
+      <WithClass classes={classes.App}>
         <button onClick={this.toggleCockpitHandler}>Remove Cockpit</button>
         {cockpit}
         {persons}
-      </div>
+      </WithClass>
     );
     // return React.createElement("div", { className: "App" }, React.createElement("h1", null, "Does this work now?"));
   }
