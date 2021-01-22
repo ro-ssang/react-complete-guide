012d4c3dd3301066e16824a1926dd055b49d5830
rossa <fashionfish@naver.com>
2021-01-22 13:50:08 +0900

    Component Update Lifecycle (for props Changes)

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,20 +1,50 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Person from "./Person/Person";
 
-const persons = (props) => {
-  console.log("[Persons.js] rendering...");
-  return props.persons.map((person, index) => {
-    return (
-      <Person
-        key={person.id}
-        changed={(event) => props.changed(event, person.id)}
-        click={() => props.clicked(index)}
-        name={person.name}
-        age={person.age}
-      />
-    );
-  });
-};
+class persons extends Component {
+  // static getDerivedStateFromProps(props, state) {
+  //   console.log("[Persons.js] getDerivedStateFromProps");
+  //   return state;
+  // }
+
+  // componentWillReceiveProps(props) {
+  //   console.log("[Persons.js] componentWullReceiveProps", props);
+  // }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("[Persons.js] ShouldComponentUpdate");
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log("[Persons.js] getSnapshotBeforeUpdate");
+    return { message: "Snapshot!" };
+  }
+
+  // componentWillUpdate() {
+
+  // }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log("[Persons.js] componentDidUpdate");
+    console.log(snapshot);
+  }
+
+  render() {
+    console.log("[Persons.js] rendering...");
+    return this.props.persons.map((person, index) => {
+      return (
+        <Person
+          key={person.id}
+          changed={(event) => this.props.changed(event, person.id)}
+          click={() => this.props.clicked(index)}
+          name={person.name}
+          age={person.age}
+        />
+      );
+    });
+  }
+}
 
 export default persons;
