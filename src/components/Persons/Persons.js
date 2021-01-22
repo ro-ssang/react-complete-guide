700788bcc4f64b086be553d0bf0eb0c36827c7af
rossa <fashionfish@naver.com>
2021-01-22 20:23:33 +0900

    Using shouldComponentUpdate for Optimization

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -14,7 +14,11 @@ class persons extends Component {
 
   shouldComponentUpdate(nextProps, nextState) {
     console.log("[Persons.js] ShouldComponentUpdate");
-    return true;
+    if (nextProps.persons !== this.props.persons) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   getSnapshotBeforeUpdate(prevProps, prevState) {
